9d9e09ef88f505104db135c00bd885412bf7610f
vikahse <57991510+vikahse@users.noreply.github.com>
2023-05-10 13:29:06 +0300

    Create Tour.py

---
+++ Tour.py
@@ -0,0 +1,10 @@
+class Tour:
+    def __init__(self, city, chat):
+        pass
+
+    def __str__(self):
+        return 'https://docs.google.com/document/d/1F51V8ePnpy4_szWdzq0vFXDxFQeXlQA-i6czRjX6T7k/edit?usp=share_link'
+
+
+def make_tour(city, users):
+    return city
